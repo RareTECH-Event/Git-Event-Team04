898f2348eb2eeafe9766568273f9c143a134b6aa
rqime <kimikisk@gmail.com>
2026-01-13 22:40:20 +0900

    main.py 選択肢2を編集したよ

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     while True:
         print("選択してください：")
         print("1: 奥野、修正したよ！！")
-        print("2: 選択肢2")
+        print("2: match,修正したよ！！")
         print("3: 選択肢3")
         print("q: 終了")
 
@@ -11,7 +11,7 @@ def main():
         if choice == "1":
             print("「奥野」が選ばれました。")
         elif choice == "2":
-            print("選択肢2が選ばれました。")
+            print("「match」が選ばれました。")
         elif choice == "3":
             print("選択肢3が選ばれました。")
         elif choice == "q":
