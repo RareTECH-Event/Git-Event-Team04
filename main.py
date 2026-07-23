24fe479c4afd9ad66bfdcada4ca30bd3395ce26a
yama-26ya <yama113926@gmail.com>
2026-07-23 22:05:19 +0900

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     while True:
         print("選択してください：")
         print("1: 猫まみれ")
-        print("2: 選択肢2")
+        print("2: やま")
         print("3: 選択肢3")
         print("q: 終了")
 
@@ -11,7 +11,7 @@ def main():
         if choice == "1":
             print("猫まみれが選ばれました。")
         elif choice == "2":
-            print("選択肢2が選ばれました。")
+            print("やまが選ばれました。")
         elif choice == "3":
             print("選択肢3が選ばれました。")
         elif choice == "q":
